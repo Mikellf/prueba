cf85d38a7ab19e55f5e049ad4fb27e5d722fbfad
Miguel L <mikellf2000@hotmail.com>
2019-11-26 21:59:32 +0100

    Este es el commit inicial de nuestro ejemplo

---
+++ src/prueba_github/Prueba_github.java
@@ -0,0 +1,9 @@
+package prueba_github;
+
+public class Prueba_github {
+
+	public static void main(String[] args) {
+		System.out.println("Esto es un ejemplo de cómo configurar Eclipse con GitHub");
+	}
+
+}
